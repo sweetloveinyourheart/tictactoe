480b275d4f69367c2769dca43ddf2b771eff6b0e
tamdatgb123 <76108242+tamdatgb123@users.noreply.github.com>
2022-05-21 16:01:46 +0700

    components user

---
+++ client/src/components/guide/guide.jsx
@@ -0,0 +1,18 @@
+const GuideComponent = () => {
+    return (  
+        <section className="guide-component">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-4">
+                        <div className="avatar">
+                            <img src="/avater.png" alt="#"/>
+                            <div className="Name">DungSenpai</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+ 
+export default GuideComponent;
---
+++ client/src/pages/guide.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/header/header";
+import GuideComponent from "../components/guide/guide";
+
+function Guide() {
+
+    return (
+      <>
+        <Header />
+        <GuideComponent />
+      </>
+    );
+  }
+  
+  export default Guide;
